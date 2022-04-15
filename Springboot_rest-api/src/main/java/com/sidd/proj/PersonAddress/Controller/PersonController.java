0de7a1aa64edf20955fd2229016670be4974cb2f
Siddharth kr <connect2sidd@hotmail.com>
2022-04-15 22:17:40 +0530

    final changes

--- Springboot_rest-api/src/main/java/com/sidd/proj/PersonAddress/Controller/PersonController.java
+++ Springboot_rest-api/src/main/java/com/sidd/proj/PersonAddress/Controller/PersonController.java
@@ -18,7 +18,6 @@ import com.sidd.proj.PersonAddress.CustomErrorHandling.NoRecordsAvaliable;
 import com.sidd.proj.PersonAddress.Entity.Person;
 import com.sidd.proj.PersonAddress.Service.PersonService;
 
-
 @RestController
 @RequestMapping("/api")
 public class PersonController {
